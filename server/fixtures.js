62460e838940504329783fd057dde5361c670ba2
Sebastian Moksnes <sebastian@moksnes.se>
2015-06-29 10:00:08 +0200

    12-1_1

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,6 +1,7 @@
-// Fixture data
+// Fixture data 
 if (Posts.find().count() === 0) {
   var now = new Date().getTime();
+  
   // create two users
   var tomId = Meteor.users.insert({
     profile: { name: 'Tom Coleman' }
@@ -10,6 +11,7 @@ if (Posts.find().count() === 0) {
     profile: { name: 'Sacha Greif' }
   });
   var sacha = Meteor.users.findOne(sachaId);
+
   var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
     userId: sacha._id,
@@ -18,6 +20,7 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 7 * 3600 * 1000),
     commentsCount: 2
   });
+
   Comments.insert({
     postId: telescopeId,
     userId: tom._id,
@@ -25,6 +28,7 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 5 * 3600 * 1000),
     body: 'Interesting project Sacha, can I get involved?'
   });
+
   Comments.insert({
     postId: telescopeId,
     userId: sacha._id,
@@ -32,6 +36,7 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 3 * 3600 * 1000),
     body: 'You sure can Tom!'
   });
+
   Posts.insert({
     title: 'Meteor',
     userId: tom._id,
@@ -40,6 +45,7 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 10 * 3600 * 1000),
     commentsCount: 0
   });
+
   Posts.insert({
     title: 'The Meteor Book',
     userId: tom._id,
@@ -55,10 +61,8 @@ if (Posts.find().count() === 0) {
       author: sacha.profile.name,
       userId: sacha._id,
       url: 'http://google.com/?q=test-' + i,
-      submitted: new Date(now - i * 3600 * 1000),
+      submitted: new Date(now - i * 3600 * 1000 + 1),
       commentsCount: 0
     });
   }
-
 }
-
