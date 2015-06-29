4fe2dfcf2981a9101f25733cda43b661771c2cb0
Sebastian Moksnes <sebastian@moksnes.se>
2015-06-29 12:33:34 +0200

    13-5

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,9 +1,9 @@
-Template.postEdit.onCreated(function() {
+Template.postEdit.onCreated(function () {
   Session.set('postEditErrors', {});
 });
 
 Template.postEdit.helpers({
-  errorMessage: function(field) {
+  errorMessage: function (field) {
     return Session.get('postEditErrors')[field];
   },
   errorClass: function (field) {
@@ -12,37 +12,36 @@ Template.postEdit.helpers({
 });
 
 Template.postEdit.events({
-  'submit form': function(e) {
+  'submit form': function (e) {
     e.preventDefault();
-    
+
     var currentPostId = this._id;
-    
+
     var postProperties = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     }
-    
+
     var errors = validatePost(postProperties);
     if (errors.title || errors.url)
       return Session.set('postEditErrors', errors);
-    
-    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+
+    Posts.update(currentPostId, { $set: postProperties }, function (error) {
       if (error) {
         // display the error to the user
         throwError(error.reason);
       } else {
-        Router.go('postPage', {_id: currentPostId});
+        Router.go('postPage', { _id: currentPostId });
       }
     });
   },
-  
-  'click .delete': function(e) {
+
+  'click .delete': function (e) {
     e.preventDefault();
-    
     if (confirm("Delete this post?")) {
       var currentPostId = this._id;
       Posts.remove(currentPostId);
-      Router.go('postsList');
+      Router.go('home');
     }
   }
 });
