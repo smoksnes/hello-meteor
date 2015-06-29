d719394d52690ca567fc4ad7ef24702cfe51136b
Sebastian Moksnes <sebastian@moksnes.se>
2015-06-29 10:51:38 +0200

    13-1

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,10 +1,17 @@
 Template.postItem.helpers({
-  ownPost: function() {
+  ownPost: function () {
     return this.userId == Meteor.userId();
   },
-  domain: function() {
+  domain: function () {
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
+  }
+});
+
+Template.postItem.events({
+  'click .upvote': function (e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
   }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,4 @@
-// Fixture data 
+// Fixture data
 if (Posts.find().count() === 0) {
   var now = new Date().getTime();
   
@@ -11,16 +11,16 @@ if (Posts.find().count() === 0) {
     profile: { name: 'Sacha Greif' }
   });
   var sacha = Meteor.users.findOne(sachaId);
-  
   var telescopeId = Posts.insert({
     title: 'Introducing Telescope',
     userId: sacha._id,
     author: sacha.profile.name,
     url: 'http://sachagreif.com/introducing-telescope/',
     submitted: new Date(now - 7 * 3600 * 1000),
-    commentsCount: 2
+    commentsCount: 2,
+    upvoters: [],
+    votes: 0
   });
-  
   Comments.insert({
     postId: telescopeId,
     userId: tom._id,
@@ -28,7 +28,6 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 5 * 3600 * 1000),
     body: 'Interesting project Sacha, can I get involved?'
   });
-  
   Comments.insert({
     postId: telescopeId,
     userId: sacha._id,
@@ -36,25 +35,26 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 3 * 3600 * 1000),
     body: 'You sure can Tom!'
   });
-  
   Posts.insert({
     title: 'Meteor',
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://meteor.com',
     submitted: new Date(now - 10 * 3600 * 1000),
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
   });
-  
   Posts.insert({
     title: 'The Meteor Book',
     userId: tom._id,
     author: tom.profile.name,
     url: 'http://themeteorbook.com',
     submitted: new Date(now - 12 * 3600 * 1000),
-    commentsCount: 0
+    commentsCount: 0,
+    upvoters: [],
+    votes: 0
   });
-  
   for (var i = 0; i < 10; i++) {
     Posts.insert({
       title: 'Test post #' + i,
@@ -62,7 +62,9 @@ if (Posts.find().count() === 0) {
       userId: sacha._id,
       url: 'http://google.com/?q=test-' + i,
       submitted: new Date(now - i * 3600 * 1000 + 1),
-      commentsCount: 0
+      commentsCount: 0,
+      upvoters: [],
+      votes: 0
     });
   }
 }
